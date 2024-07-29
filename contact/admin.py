0f3d6901e68e5254ae7e357f9b328cd9576383d9
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-28 21:45:46 -0300

    Update admin

--- contact/admin.py
+++ contact/admin.py
@@ -6,7 +6,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
     list_per_page = 10
-    list_filter = 'created_date',
+    # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_max_show_all = 100
     list_editable = 'first_name', 'last_name',
