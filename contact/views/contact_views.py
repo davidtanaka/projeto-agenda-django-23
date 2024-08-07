9a0fcbb1c34735b4956f637a6dd2a158340eab2b
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-08-06 22:26:56 -0300

    Mudando a ordem da ordenagem do id

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,7 +4,7 @@ from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects .filter(show=True).order_by('-id')[10:20]
+    contacts = Contact.objects .filter(show=True).order_by('id')[10:20]
 
     context = {
         'contacts': contacts,
