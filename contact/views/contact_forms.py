4c1c3fb82d1251cb97b1204b7256d3bd1e6663f8
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-08-11 19:52:21 -0300

    csrf_token - a proteção do Django contra ataques Cross Site Request Forgery, Usando request.method e request.POST para saber quando o formulário é postado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,6 +6,12 @@ from contact.models import Contact
 
 
 def create(request):
+    if request.method == 'POST':
+        request.POST.get('first_name')
+        request.POST.get('last_name')
+        # print(request.POST.get('first_name'))
+        # print(request.POST.get('last_name'))
+
     context = {
 
     }
