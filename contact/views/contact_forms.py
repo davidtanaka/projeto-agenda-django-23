bb8f9c90f45669d1fb1a438daebf5b8edc0777da
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-08-14 15:42:28 -0300

    Verificando form.is_valid, salvando com form.save e redirecionando a página

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,10 +1,18 @@
-from django.shortcuts import  render
+from django.shortcuts import  redirect, render
 
 from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
-        context = { 'form': ContactForm(data=request.POST) }
+        form = ContactForm(data=request.POST)
+        context = { 'form': form }
+
+        if form.is_valid():
+            # contacts_test = form.save(commit=False) # Isso não vai salvar na base de dados
+            # Mas com o código abaixo vou salvar essa váriavel:
+            # contacts_test.show=False
+            form.save()
+            return redirect('contact:create')
 
         return render(
             request,
