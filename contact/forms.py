a4aaf2540711d0d1c9a4683f199a88f6f7f611d6
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-08-14 14:40:27 -0300

    Adicionando mais campos no formulário de contato

--- contact/forms.py
+++ contact/forms.py
@@ -22,7 +22,8 @@ class ContactForm(forms.ModelForm):
 
     class Meta:
         model = models.Contact
-        fields = 'first_name', 'last_name', 'phone',
+        fields = 'first_name', 'last_name', 'phone',\
+                'email', 'description', 'phone'
         widgets = {
             'first_name': forms.TextInput(
                 attrs={
